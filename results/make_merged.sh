636650e53cc4e2e2043074f0e9a3038e933fec5f
lgbouma <lgbouma@mit.edu>
2017-12-05 14:12:54 -0500

    update results plots

--- results/make_merged.sh
+++ results/make_merged.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-pdftk \
-  errcases_rate_density_vs_radius_model_1_brokenx.pdf \
-  errcases_rate_density_vs_radius_model_2.pdf \
-  errcases_rate_density_vs_radius_logs_model_2.pdf \
-  errcases_rate_density_vs_radius_model_3_Lambda2_0.5.pdf \
-  errcases_rate_density_vs_radius_logs_model_3_Lambda2_0.5.pdf \
-  errcases_rate_density_vs_radius_model_3_withtext_Lambda2_0.5.pdf \
-  output errcases_merged.pdf
 
 pdftk \
   rate_density_vs_radius_model_1_brokenx.pdf \
   rate_density_vs_radius_model_2.pdf \
   rate_density_vs_radius_logs_model_2.pdf \
-  rate_density_vs_radius_model_3_Lambda2_0.5.pdf \
-  rate_density_vs_radius_logs_model_3_Lambda2_0.5.pdf \
-  rate_density_vs_radius_model_3_withtext_Lambda2_0.5.pdf \
+  rate_density_vs_radius_model_3_Zsub2_0.5.pdf \
+  rate_density_vs_radius_logs_model_3_Zsub2_0.5.pdf \
+  rate_density_vs_radius_model_3_withtext_Zsub2_0.5.pdf \
   output rates_merged.pdf
+
+# OUTDATED: "error cases" plots.
+
+#pdftk \
+#  errcases_rate_density_vs_radius_model_1_brokenx.pdf \
+#  errcases_rate_density_vs_radius_model_2.pdf \
+#  errcases_rate_density_vs_radius_logs_model_2.pdf \
+#  errcases_rate_density_vs_radius_model_3_Zsub2_0.5.pdf \
+#  errcases_rate_density_vs_radius_logs_model_3_Zsub2_0.5.pdf \
+#  errcases_rate_density_vs_radius_model_3_withtext_Zsub2_0.5.pdf \
+#  output errcases_merged.pdf
+
+
