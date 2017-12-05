0785436891e94822836c9d55e9ce638b4c0eee1e
lgbouma <lgbouma@mit.edu>
2017-12-05 14:12:11 -0500

    add helper script to run all models

---
+++ src/run_all_models.sh
@@ -0,0 +1,5 @@
+python numerical_models.py --modelnumber 1 --ZsubTwo 0.5 --binaryfrac 0.1
+python numerical_models.py --modelnumber 2 --ZsubTwo 0.5 --binaryfrac 0.44
+python numerical_models.py --modelnumber 3 --ZsubTwo 0.5 --binaryfrac 0.44
+python numerical_models.py --modelnumber 4 --ZsubTwo 0.5 --binaryfrac 0.44
+python numerical_models.py --modelnumber 5 --ZsubTwo 0.5 --binaryfrac 0.1
