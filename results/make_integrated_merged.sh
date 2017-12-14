a0b3fb64eda95d08d397a29ae709df30ead9fc93
lgbouma <lgbouma@mit.edu>
2017-12-14 11:40:36 -0500

    add wrapper to merge integrator plots.

---
+++ results/make_integrated_merged.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# general models
+pdftk \
+  int_rate_density_vs_radius_logs_model_5.pdf \
+  int_rate_density_vs_radius_logs_model_6.pdf \
+  int_rate_density_vs_radius_model_3_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_logs_model_3_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_4_xcut.pdf \
+  output int_rate_density_merged.pdf
+
+# as function of Z_2
+pdftk \
+  int_rate_density_vs_radius_logs_model_3_Zsub2_0.00_rpu_22.5.pdf \
+  int_rate_density_vs_radius_logs_model_3_Zsub2_0.25_rpu_22.5.pdf \
+  int_rate_density_vs_radius_logs_model_3_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.00_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.25_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_22.5.pdf \
+  output int_rate_density_merged_fn_of_Z2.pdf
+
+#as function of rpu
+pdftk \
+  int_rate_density_vs_radius_logy_model_3_Zsub2_0.50_rpu_15.0.pdf \
+  int_rate_density_vs_radius_logy_model_3_Zsub2_0.50_rpu_17.5.pdf \
+  int_rate_density_vs_radius_logy_model_3_Zsub2_0.50_rpu_20.0.pdf \
+  int_rate_density_vs_radius_logy_model_3_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_logy_model_3_Zsub2_0.50_rpu_25.0.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_15.0.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_17.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_20.0.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_22.5.pdf \
+  int_rate_density_vs_radius_model_3_withtext_Zsub2_0.50_rpu_25.0.pdf \
+  output int_rate_density_merged_fn_of_rpu.pdf
+
