3497474bbef599bb8c866ba289b6bb8e0a4cbfcf
lgbouma <lgbouma@mit.edu>
2018-03-01 09:35:44 -0500

    rename figs for aas convention

--- paper/figures/collect_results.sh
+++ paper/figures/collect_results.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-cp ../../results/visualize_volumes.pdf .
-cp ../../results/occ_rate_vs_radius_model_1_brokenx.pdf .
-cp ../../results/mass_ratio_distribution.pdf .
-cp ../../results/int_rate_density_vs_radius_model_3_rpu_22.5_manyZs.pdf .
-cp ../../results/int_occ_rate_vs_radius_model_3_rpu_22.5_manyZs.pdf .
-cp ../../results/ndet_vs_radius_logx_model_3_fraclines_rpu_22.5_manyZs.pdf .
-cp ../../results/int_rate_density_vs_radius_model_4_xcut_manyZs.pdf .
-cp ../../results/int_occ_rate_vs_radius_model_4_xcut_manyZs.pdf .
-cp ../../results/int_rate_density_vs_radius_model_7_rpu_22.5_manyZs.pdf .
+cp ../../results/visualize_volumes.pdf f1.pdf
+cp ../../results/occ_rate_vs_radius_model_1_brokenx.pdf f2.pdf
+cp ../../results/mass_ratio_distribution.pdf f3.pdf
+cp ../../results/int_rate_density_vs_radius_model_3_rpu_22.5_manyZs.pdf f4a.pdf
+cp ../../results/int_occ_rate_vs_radius_model_3_rpu_22.5_manyZs.pdf f4b.pdf
+cp ../../results/ndet_vs_radius_logx_model_3_fraclines_rpu_22.5_manyZs.pdf f5.pdf
+cp ../../results/int_rate_density_vs_radius_model_4_xcut_manyZs.pdf f6a.pdf
+cp ../../results/int_occ_rate_vs_radius_model_4_xcut_manyZs.pdf f6b.pdf
+cp ../../results/int_rate_density_vs_radius_model_7_rpu_22.5_manyZs.pdf f7.pdf
